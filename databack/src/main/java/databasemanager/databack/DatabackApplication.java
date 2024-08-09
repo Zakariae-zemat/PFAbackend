21f6c71b2c24bb26d10bc15c12407847783e69ae
Zakariae-zemat <153006293+Zakariae-zemat@users.noreply.github.com>
2024-08-09 12:14:00 +0100

    Add files via upload

---
+++ databack/src/main/java/databasemanager/databack/DatabackApplication.java
@@ -0,0 +1,13 @@
+package databasemanager.databack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabackApplication.class, args);
+	}
+
+}
