36eab9b22a367361a0eb18bbff00636c06e09c2d
Zakariae-zemat <153006293+Zakariae-zemat@users.noreply.github.com>
2024-10-31 03:07:28 +0100

    Update JwtRequestFilter.java

--- src/main/java/backend/backend/security/JwtRequestFilter.java
+++ src/main/java/backend/backend/security/JwtRequestFilter.java
@@ -15,7 +15,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
